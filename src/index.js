59ca8e002788e216f71a0e3282c5325f8dd72ab8
Zijian Cao <algebra18874@gmail.com>
2024-10-12 16:35:14 -0400

    CLI formatted output

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 const vorpal = require('vorpal')();
+const Table = require('cli-table')
 const Blockchain = require('./blockchain')
 const blockchain = new Blockchain()
 
+
+function formatLog(data) {
+  if (!Array.isArray(data)) {
+    data = [data]
+  }
+  const first = data[0]
+  const head = Object.keys(first)
+  const table = new Table({
+    head: head
+  , colWidths: new Array(head.length).fill(15)
+  })
+  const res = data.map(v=>{
+    return head.map(h=>v[h])
+  })
+  
+  table.push(...res);
+  console.log(table.toString());
+}
+
+
 vorpal
   .command('mine', 'add new blocks to the chain')
-  .action(function(args, callback) {
+  .action(function (args, callback) {
     const newBlock = blockchain.mine()
-    if(newBlock){
-        console.log(newBlock)
+    if (newBlock) {
+      formatLog(newBlock)
     }
     callback();
   });
 
 vorpal
   .command('chain', 'add new blocks to the chain')
-  .action(function(args, callback) {  
-    this.log(blockchain.blockchain);
+  .action(function (args, callback) {
+    formatLog(blockchain.blockchain);
     callback();
   });
 
@@ -26,8 +47,8 @@ vorpal
 //     callback();
 //   });
 
-  console.log('weclome to chainblock')
-  vorpal.exec('help')
+console.log('weclome to chainblock')
+vorpal.exec('help')
 vorpal
   .delimiter('chian => ')
   .show();
