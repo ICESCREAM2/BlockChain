552a20afebc78c3bc2de13c3d64bb368095b640b
Zijian Cao <algebra18874@gmail.com>
2024-10-14 23:32:52 -0400

    add rsa algorithm

---
+++ src/rsa.js
@@ -0,0 +1,60 @@
+//public and private key 
+//public key as address(simplified)
+
+let fs = require('fs')
+let EC = require('elliptic').ec;
+
+// Create and initialize EC context
+// (better do it once and reuse it)
+let ec = new EC('secp256k1');
+
+// Generate keys
+let keypair = ec.genKeyPair();
+
+const keys = generateKeys()
+// console.log(keys)
+
+function getPub(prv) {
+    return ec.keyFromPrivate(prv).getPublic('hex').toString()
+}
+function generateKeys() {
+    const fileName = './wallet.json'
+    try {
+        let res = JSON.parse(fs.readFileSync(fileName))
+        //validation
+        if (res.prv && res.pub && getPub(res.prv) == res.pub) {
+            keypair = ec.keyFromPrivate(res.prv)
+            return res
+        } else {
+            throw 'not valid data in wallet.json'
+        }
+    } catch (error) {
+        const res = {
+            prv: keypair.getPrivate('hex').toString(),
+            pub: keypair.getPublic('hex').toString()
+        }
+        fs.writeFileSync(fileName, JSON.stringify(res))
+        return res
+    }
+
+}
+
+
+function sign({from,to,amount}){
+    const bufferMsg = Buffer.from(`${from}-${to}-${amount}`)
+    let signature = Buffer.from(keypair.sign(bufferMsg).toDER()).toString('hex')
+    return signature
+}
+
+function verify({from,to,amount},pub){
+    const keypairTemp = ec.keyFromPublic(pub,'hex')
+    const bufferMsg = Buffer.from(`${from}-${to}-${amount}`)
+    return keypairTemp.verify(bufferMsg,signature)
+}
+const trans = {from:'me',to:'you',amount:100}
+const trans2 = {from:'me',to:'you',amount:101}
+const signature = sign(trans)
+console.log(signature)
+trans.signature = signature
+const isVerify = verify(trans,keys.pub)
+console.log(isVerify)
